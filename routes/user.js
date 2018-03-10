cfaf864663ebb531159674b51f1ca019f9f65b96
Amal Sudama <cds.sudama@gmail.com>
2018-03-10 08:20:06 -0500

    implement users routes

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,30 @@
- const router = require('express').Router();
+const router = require('express').Router() // eslint-disable-line new-cap
+const {User, Page} = require('../models')
+const {userList, userPages} = require('../views')
 
- module.exports = router;
+router.get('/', async (req, res, next) => {
+  //
+  console.log('users  root')
+  try {
+    const authors = await User.findAll()
+    res.send(userList(authors))
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const author = await User.findById(req.params.id)
+    const pages = await Page.findAll({
+      where: {
+        authorId: req.params.id
+      }
+    })
+    res.send(userPages(author, pages))
+  } catch (err) {
+    next(err)
+  }
+})
+
+module.exports = router
