8e2299fd7f28ada25bc14f6a74b9bb3aadf0991b
Amal Sudama <cds.sudama@gmail.com>
2018-03-09 20:16:28 -0500

    fix redirect after POST page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,43 +1,43 @@
-const {addPage, main, wikiPage} = require('../views');
-const router = require('express').Router();
-const { Page } = require('../models');
+const {addPage, main, wikiPage} = require('../views')
+const router = require('express').Router() // eslint-disable-line new-cap
+const {Page} = require('../models')
 
 router.get('/', (req, res, next) => {
-  //res.send('Hello from Wiki!');
-  res.send(main());
-});
+  res.send(main())
+})
 
 router.get('/add', (req, res, next) => {
-  res.send(addPage());
-});
+  res.send(addPage())
+})
 
 router.get('/:slug', async (req, res, next) => {
-  const {slug} = req.params;
+  const {slug} = req.params
   try {
     const page = await Page.findOne({
       where: {
         slug
       }
     })
-    console.log(wikiPage(page, 'Daisy'))
-    res.send(wikiPage(page, 'Daisy'));
+
+    res.send(wikiPage(page, 'Daisy'))
   } catch (error) {
     next(error)
   }
 })
 
 router.post('/', async (req, res, next) => {
-  const {title, content, status} = req.body;
-  console.log(JSON.stringify(req.body));
+  const {title, content, status} = req.body
   const page = new Page({
-  title, content, status
+    title,
+    content,
+    status
   })
   try {
-    await page.save();
-    res.redirect(`/${page.slug}`);
+    await page.save()
+    res.redirect(`/wiki/${page.slug}`)
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-module.exports = router;
+module.exports = router
