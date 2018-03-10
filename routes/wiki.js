2b0cad537d4669c7a862f371fd2ae7a1d67bf49f
Amal Sudama <cds.sudama@gmail.com>
2018-03-10 08:21:23 -0500

    refactor to take advantage of Sequelize Model.create ability to grab properties from object

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,6 +1,6 @@
 const {addPage, main, wikiPage} = require('../views')
 const router = require('express').Router() // eslint-disable-line new-cap
-const {Page} = require('../models')
+const {Page, User} = require('../models')
 
 router.get('/', (req, res, next) => {
   res.send(main())
@@ -26,14 +26,17 @@ router.get('/:slug', async (req, res, next) => {
 })
 
 router.post('/', async (req, res, next) => {
-  const {title, content, status} = req.body
-  const page = new Page({
-    title,
-    content,
-    status
-  })
+  const {author: name, email} = req.body
+
   try {
-    await page.save()
+    // eslint-disable-next-line no-unused-vars
+    const [author, _] = await User.findOrCreate({
+      where: {name, email}
+    })
+
+    const page = await Page.create(req.body)
+    page.setAuthor(author)
+
     res.redirect(`/wiki/${page.slug}`)
   } catch (error) {
     next(error)
